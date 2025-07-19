c40f691606ba40bba925164f451706413b66f122
Sarat Kumar Kalagara <saratkumarkalagara2398s@gmail.com>
2025-07-19 09:29:22 +0530

    Update User.jsx

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import UserSideBar from '../components/userSideBar';
+
 const User = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-  <UserSideBar />
-  <main className="flex-grow p-4">
-    <Outlet />
-  </main>
-</div>
-
+    <div className="flex flex-col md:flex-row min-h-screen">
+      <div className="w-full md:w-1/4">
+        <UserSideBar />
+      </div>
+      <main className="flex-grow p-4">
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
