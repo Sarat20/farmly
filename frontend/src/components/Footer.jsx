280c807fb90c5fc491ddaa68dd189337cb7fb9d6
Sarat Kumar Kalagara <saratkumarkalagara2398s@gmail.com>
2025-07-19 09:33:03 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,55 +3,47 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex flex-col">
-     
       {/* --this is footer top */}
-      <div className="bg-gray-700 text-white flex flex-row px-4 justify-between py-4">
-        <div>
-          <span className="text-2xl font-bold">Get to Know Us</span>
-          <ul>About Farmly </ul>
+      <div className="bg-gray-700 text-white flex flex-col md:flex-row flex-wrap px-4 justify-between py-4 gap-6">
+        <div className="min-w-[200px]">
+          <span className="text-xl font-bold block mb-2">Get to Know Us</span>
+          <ul>About Farmly</ul>
           <ul>Farmly Releases</ul>
           <ul>Farmly Science</ul>
         </div>
 
-        <div>
-         
-          <span className="text-2xl font-bold"> Connect with Us</span>
+        <div className="min-w-[200px]">
+          <span className="text-xl font-bold block mb-2">Connect with Us</span>
           <ul>Facebook</ul>
           <ul>Twitter(x)</ul>
           <ul>Instagram</ul>
         </div>
 
-        <div>
-          
-          <span className="text-2xl font-bold"> Make Money With Us</span>
+        <div className="min-w-[200px]">
+          <span className="text-xl font-bold block mb-2">Make Money With Us</span>
           <ul>Sell On Farmly</ul>
           <ul>Farmly Global Selling</ul>
-          <ul>Suppy to Farmly</ul>
-          <ul>Become an Affliate</ul>
+          <ul>Supply to Farmly</ul>
+          <ul>Become an Affiliate</ul>
         </div>
 
-        <div>
-          
-          <span className="text-2xl font-bold">Let Us help You</span>
+        <div className="min-w-[200px]">
+          <span className="text-xl font-bold block mb-2">Let Us Help You</span>
           <ul>Your Account</ul>
           <ul>Help</ul>
         </div>
-
       </div>
 
-      {/* this is about footer end */}
-
-      <div className="flex flex-col items-center bg-black text-white">
-        <div className="flex flex-row space-x-7 mt-3">
-
-          <p>Conditions of Use and Sell</p> 
+      {/* --this is footer bottom */}
+      <div className="flex flex-col items-center bg-black text-white text-center">
+        <div className="flex flex-col md:flex-row md:space-x-7 mt-3 space-y-1 md:space-y-0">
+          <p>Conditions of Use and Sell</p>
           <p>Privacy Note</p>
-          <p>Interest Based Adds</p>
+          <p>Interest Based Ads</p>
         </div>
         <div className="py-3">
-            <p>2025-, Farmly.com, Inc.  or its affliates</p>
+          <p>2025-, Farmly.com, Inc. or its affiliates</p>
         </div>
-
       </div>
     </div>
   );
